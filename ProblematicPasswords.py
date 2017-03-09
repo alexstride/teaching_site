a8e4b0e52d48be4608a7142a61cdc22e123f9908
alexstride <stridealexander@gmail.com>
2017-03-09 16:44:42 +0000

    Update ProblematicPasswords.py

--- ProblematicPasswords.py
+++ ProblematicPasswords.py
@@ -1,14 +1,17 @@
 #This is a version of the program which some of you completed for prep.
 #There are a number of errors in the code. Answer the questions on the relevant google form and fix the code as you go.
 
-password = "cabbage
+#The first error is in the following line
+password = cabbage
 
+#The second error is in the following line
 attempt = input("Please enter your password: "
 
 wrongAttempts = 0
 
 while attempt != password:
 	wrongAttempts += 1
+	# the final error is in the following line
 	if wrongAttempts < 3:
 		print("Too many guesses; you are locked out")
 		break
